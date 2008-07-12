6e685d57728e588a6cb7813cedffdffb2c381b59
p.panconi <p.panconi@b317df8c-944d-0410-a137-0700518723e3>
2008-07-12 15:39:23 +0000

    Create module blite-se Parser

---
+++ trunk/blite-se/blite-se-parser/src/main/java/it/unifi/dsi/blitese/parser/App.java
@@ -0,0 +1,13 @@
+package it.unifi.dsi.blitese.parser;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
